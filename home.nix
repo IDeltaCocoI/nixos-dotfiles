0950e46f0c47e03abf782dfbb41f2b5dbac747c7
IDeltaCocoI <corentin.louis74@gmail.com>
2025-09-15 21:25:53 +0200

    home.nix

--- home.nix
+++ home.nix
@@ -9,10 +9,12 @@
 
   programs.bash.enable = true;
   programs.bash.shellAliases = {
+    nr = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#corentin";
   };
 
   home.file.".config/i3".source = ./config/i3;
   home.file.".config/nvim".source = ./config/nvim;
+  home.file.".config/alacritty".source = ./config/alacritty;
 
   home.packages = with pkgs; [
     neovim
