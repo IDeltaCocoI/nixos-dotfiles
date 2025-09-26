5c8addc4d9ca86f3283aa8d1e172bc6e8066c707
IDeltaCocoI <corentin.louis74@gmail.com>
2025-09-26 21:54:08 +0200

    start

--- config/i3/start.sh
+++ config/i3/start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-rm -rf ~/.config/i3
-nix shell github:nix-community/home-manager --command home-manager switch --flake ~/nixos-dotfiles#corentin
+alacritty -e bash -c "
+echo 'Suppression de ~/.config/i3...';
+rm -rf ~/.config/i3;
+
+echo 'Lancement de Home Manager...';
+nix shell github:nix-community/home-manager --command home-manager switch --flake ~/nixos-dotfiles#corentin;
+
+echo 'Reload de i3...';
+i3-msg reload
+
+echo 'Terminé !';
+read -p 'Appuyez sur Entrée pour fermer le terminal...'
+"
 
