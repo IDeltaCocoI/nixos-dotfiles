d505712ff4d0f835722e3eb8691d496e1814dd66
Corentin Louis <corentin.louis@epita.fr>
2025-09-29 13:21:25 +0200

    ada

---
+++ reload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r ~/nixos-dotfiles/config/i3 ~/.config
+
+nix shell github:nix-community/home-manager --command home-manager switch --flake ~/nixos-dotfiles#epita;
+i3-msg reload
+
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REPO="git@github.com:IDeltaCocoI/nixos-dotfiles.git"
-TARGET="~/afs/nixos-dotfiles"
-
-[ -d "$TARGET" ] && rm -rf "$TARGET"
-
-git clone "$REPO" "$TARGET"
-
