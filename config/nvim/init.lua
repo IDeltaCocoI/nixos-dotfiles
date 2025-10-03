ed513130cd5e81c2ea2a92ed421c14d171db58d1
Corentin Louis <corentin.louis@epita.fr>
2025-10-03 12:00:28 +0200

    a

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,8 +7,8 @@ require("colors").setup()
 vim.o.number = true           -- show line numbers
 vim.o.relativenumber = true   -- show relative line numbers
 vim.o.expandtab = true        -- convert tabs to spaces
-vim.o.shiftwidth = 2          -- spaces per indent
-vim.o.tabstop = 2             -- spaces per tab
+vim.o.shiftwidth = 4          -- spaces per indent
+vim.o.tabstop = 4             -- spaces per tab
 vim.o.smartindent = true      -- smart auto-indentation
 
 -- ==============================
