2394fd0e46d71141e64e2fe4afe6e7f68cc0abb3
IDeltaCocoI <corentin.louis74@gmail.com>
2025-10-10 23:37:12 +0200

    criterion

--- configuration.nix
+++ configuration.nix
@@ -1,69 +1,86 @@
 { config, lib, pkgs, ... }:
 {
-  imports =
-    [
-      ./hardware-configuration.nix
+    imports =
+        [
+        ./hardware-configuration.nix
+        ];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "Nixos";
+    networking.networkmanager.enable = true;
+    programs.nm-applet.enable = true;
+
+    time.timeZone = "Europe/Paris";
+
+    services.xserver = {
+        enable = true;
+        autoRepeatDelay = 200;
+        autoRepeatInterval = 35;
+        windowManager.i3.enable = true;
+    };
+
+    services.displayManager.ly.enable = true;
+
+    users.users.corentin = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        packages = with pkgs; [
+            tree
+        ];
+    };
+
+    documentation.man.enable = true;
+    documentation.dev.enable = true;
+
+    programs.firefox.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+    environment.systemPackages = with pkgs; [
+        wget
+        alacritty
+        btop
+        feh
+        man
+        glibc.dev
+        man-pages
+        bash
+        coreutils
+        findutils
+        diffutils
+        util-linux
+        gawk
+        less
+        procps
+        shadow
+        which
+        file
+        rofi
+        pcmanfm
+        spotify
+        pulseaudio
+        discord
+        git
+        criterion
+        neofetch 
+        clang
+        clang-tools
+        llvm
+        gnumake
+        gdb
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "Nixos";
-  networking.networkmanager.enable = true;
-  programs.nm-applet.enable = true;
-
-  time.timeZone = "Europe/Paris";
-
-  services.xserver = {
-    enable = true;
-    autoRepeatDelay = 200;
-    autoRepeatInterval = 35;
-    windowManager.i3.enable = true;
-  };
-
-  services.displayManager.ly.enable = true;
-
-  users.users.corentin = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      tree
+    fonts.packages = with pkgs; [
+        nerd-fonts.jetbrains-mono
     ];
-  };
 
-  programs.firefox.enable = true;
+    hardware.enableAllFirmware = true;
+    boot.extraModulePackages = [
+        pkgs.linuxPackages.rtl8812au
+    ];
 
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    wget
-    alacritty
-    btop
-    feh
-    man
-    rofi
-    pcmanfm
-    spotify
-    pulseaudio
-    discord
-    git
-    neofetch 
-    clang
-    clang-tools
-    llvm
-    gnumake
-    gdb
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
-
-  hardware.enableAllFirmware = true;
-  boot.extraModulePackages = [
-    pkgs.linuxPackages.rtl8812au
-  ];
-  
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "25.05";
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    system.stateVersion = "25.05";
 }
 
