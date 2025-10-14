216e83785bff967aa109ade66e7c13b2883e73c5
IDeltaCocoI <corentin.louis74@gmail.com>
2025-10-14 15:40:12 +0200

    wallpaper

--- wallpapers/wallpapers.sh
+++ wallpapers/wallpapers.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SETTER="feh --bg-scale"
-DIR="$(pwd)"
+DIR="$HOME/nixos-dotfiles/wallpapers"
 IMAGE=$(find "$DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) | shuf -n 1)
 
 $SETTER "$IMAGE"
