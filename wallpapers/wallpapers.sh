5c8ae617790ffe8ac7df1071eb1e10f105a3192a
IDeltaCocoI <corentin.louis74@gmail.com>
2025-10-15 15:01:50 +0200

    wallpaper

--- wallpapers/wallpapers.sh
+++ wallpapers/wallpapers.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+IMG="hollow_knight.png"
 SETTER="feh --bg-scale"
-DIR="$HOME/nixos-dotfiles/wallpapers"
+DIR="$HOME/nixos-dotfiles/wallpapers/"
 IMAGE=$(find "$DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) | shuf -n 1)
 
-$SETTER "$IMAGE"
+if [ "$IMG" != "" ]; then
+    $SETTER "$DIR$IMG"
+else
+    $SETTER "$IMAGE"
+fi
 
